a6798e4844c861f8b9d361b29a2ca76c972dabf8
nyeri9999 <neoyeo66@gmail.com>
2026-01-17 00:29:38 +0900

    [MOODCODE] feat : 초기 프로젝트 설정

---
+++ src/main/java/com/devcrew/moodcode/MoodcodeApplication.java
@@ -0,0 +1,13 @@
+package com.devcrew.moodcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodcodeApplication.class, args);
+	}
+
+}
