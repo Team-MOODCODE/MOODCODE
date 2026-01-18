53d6fba6b1a284235978683912ed472225cdc44a
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-01-18 20:10:51 +0900

    [MOODCODE] feat: 장바구니 조회 및 중복 체크 구현 - userId로 장바구니 조회 기능 - userId와 ProductOptionId 중복 여부 확인 기능

---
+++ src/main/java/com/devcrew/moodcode/domain/cart/repository/CartRepository.java
@@ -0,0 +1,15 @@
+package com.devcrew.moodcode.domain.cart.repository;
+
+import com.devcrew.moodcode.domain.cart.Cart;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+  Optional<Cart> findByUserId(Long userId);
+  Boolean existsByUserIdAndProductOptionId(Long userId, Long productOptionId);
+
+}
