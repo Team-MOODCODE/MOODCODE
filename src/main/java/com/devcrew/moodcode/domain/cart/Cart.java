6a4420e0c5318e4d93115ecea0b27c248b8ea888
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-01-23 19:23:20 +0900

    [MOODCODE] feat: 데이터베이스 설계 변경으로 인한 수정 및 cartItems 필드 추가
    - 기존: cart
    - 수정 후: cart - cartItem

--- src/main/java/com/devcrew/moodcode/domain/cart/Cart.java
+++ src/main/java/com/devcrew/moodcode/domain/cart/Cart.java
@@ -1,64 +1,35 @@
 package com.devcrew.moodcode.domain.cart;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import java.time.LocalDateTime;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-//@Table(
-//    name = "cart",
-//    uniqueConstraints = {
-//        @UniqueConstraint(
-//            name = "uk_user_product_option", // 제약 조건 이름
-//            columnNames = {"user_id", "product_option_id"}
-//        )
-//    }
-//)
 public class Cart {
   @Id
   @Column(name = "cart_id")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long cartId;
+  private Long id;
 
-  // Default 1
-  @Column(nullable = false, columnDefinition = "int default 1")
-  private int count = 1;
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
 
-  @UpdateTimestamp // 변경 시 자동 업데이트.
-  @Column(name = "updated_at")
-  private LocalDateTime updatedAt;
+  @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<CartItem> cartItems;
 
-  // User와 ProductOption 클래스가 구현되면 작성.
-  // @OneToOne
-  // private User userId;
-  // @OneToMany
-  // private List<ProductOption> productOptions;
-
-  // 장바구니에 상품 추가
-  public void add(Long productOptionId) {
-    productOptions.add(productOptionId);
-    updatedAt = LocalDateTime.now();
-  }
-
-  // 장바구니 상품 삭제
-  public void remove(Long productOptionId) {
-    productOptions.remove(productOptionId);
-    updatedAt = LocalDateTime.now();
-  }
-
-  public void addCount() {
-    count++;
-  }
 }
