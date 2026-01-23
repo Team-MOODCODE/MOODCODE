ebb2bef059edc9dd0e8a41e7330f0359c6b525c7
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-01-23 19:34:23 +0900

    [MOODCODE] feat: 존재 유무 기능 삭제
    - 데이터베이스 설계 변경으로 인해 존재 유무 기능을 CartItemRepository 역할로 변경

--- src/main/java/com/devcrew/moodcode/domain/cart/repository/CartRepository.java
+++ src/main/java/com/devcrew/moodcode/domain/cart/repository/CartRepository.java
@@ -1,7 +1,6 @@
 package com.devcrew.moodcode.domain.cart.repository;
 
 import com.devcrew.moodcode.domain.cart.Cart;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-  Optional<Cart> findByUserId(Long userId);
-  Boolean existsByUserIdAndProductOptionId(Long userId, Long productOptionId);
+  Optional<Cart> findByUserId(Long user);
 
 }
