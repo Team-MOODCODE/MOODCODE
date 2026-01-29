cf09fa0936127c9984719926dbae70088b80ad38
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-01-29 22:58:50 +0900

    [MOODCODE] chore: JPA Auditing 활성화 설정 추가
    - 자동 생성/수정일 관리: @EnableJpaAuditing 설정을 추가하여 엔티티의 생성 시각(CreatedAt) 및 수정 시각(ModifiedAt)을 자동으로 기록

--- src/main/java/com/devcrew/moodcode/MoodcodeApplication.java
+++ src/main/java/com/devcrew/moodcode/MoodcodeApplication.java
@@ -2,7 +2,9 @@ package com.devcrew.moodcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MoodcodeApplication {
 
