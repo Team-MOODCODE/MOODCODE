0b6beabd75f840a02fbd424dd31dd2545b8c40ff
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-01-29 22:57:15 +0900

    [MOODCODE] feat: 위시리스트 브랜드 조회 응답 DTO 생성
    - 정적 팩토리 메서드 구현: from() 메서드를 통해 Brand 엔티티 리스트를 응답용 DTO 리스트로 변환하는 로직을 캡슐화.
    
    - 중첩 구조 설계: 전체 리스트를 감싸는 FindWishlistBrandsResponse와 개별 브랜드 정보를 담는 내부 Record를 분리하여 확장성을 높임.
    
    - Stream 활용: 엔티티 리스트를 순회하며 필요한 필드(ID, 브랜드명, 상품명, 좋아요 수)만 추출하여 변환하도록 구현

---
+++ src/main/java/com/devcrew/moodcode/domain/wishlist/service/response/FindWishlistBrandsResponse.java
@@ -0,0 +1,29 @@
+package com.devcrew.moodcode.domain.wishlist.service.response;
+
+import com.devcrew.moodcode.domain.wishlist.Brand;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record FindWishlistBrandsResponse(List<FindWishlistBrandResponse> responses) {
+
+  public static FindWishlistBrandsResponse from(
+      List<Brand> brands) {
+    List<FindWishlistBrandResponse> responses = brands.stream().map(brand -> {
+      return FindWishlistBrandResponse.builder()
+          .brandId(brand.getId())
+          .brandName(brand.getName())
+          .brandLikeCount(brand.getLikeCount())
+          .build();
+    }).toList();
+
+    return FindWishlistBrandsResponse.builder()
+        .responses(responses)
+        .build();
+
+  }
+
+  @Builder
+  public static record FindWishlistBrandResponse(Long brandId, String brandName, int brandLikeCount) {}
+
+}
