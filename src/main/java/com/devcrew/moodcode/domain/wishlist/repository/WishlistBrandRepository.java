5ddcace23e355a2893a8471c2d85e577e16499c6
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-01-29 22:36:45 +0900

    [MOODCODE] feat: WishlistBrandRepository 구현

---
+++ src/main/java/com/devcrew/moodcode/domain/wishlist/repository/WishlistBrandRepository.java
@@ -0,0 +1,8 @@
+package com.devcrew.moodcode.domain.wishlist.repository;
+
+import com.devcrew.moodcode.domain.wishlist.WishlistBrand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WishlistBrandRepository extends JpaRepository<WishlistBrand, Long> {
+
+}
