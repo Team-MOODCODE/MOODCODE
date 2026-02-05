a17f5c449c08fa515aec3cdf4f17eb884b39c6a0
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-02-05 16:44:32 +0900

    [MOODCODE] fix: 연관 관계 수정에 따른 비즈니스 로직 수정

--- src/main/java/com/devcrew/moodcode/domain/wishlist/service/WishlistBrandService.java
+++ src/main/java/com/devcrew/moodcode/domain/wishlist/service/WishlistBrandService.java
@@ -7,6 +7,8 @@ import com.devcrew.moodcode.domain.wishlist.repository.BrandRepository;
 import com.devcrew.moodcode.domain.wishlist.repository.WishlistBrandRepository;
 import com.devcrew.moodcode.domain.wishlist.repository.WishlistRepository;
 import com.devcrew.moodcode.domain.wishlist.service.response.FindWishlistBrandsResponse;
+import com.devcrew.moodcode.global.error.ErrorCode;
+import com.devcrew.moodcode.global.error.exception.BusinessException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,27 +25,20 @@ public class WishlistBrandService implements WishlistService {
   public void like(Long userId, Long brandId) {
     Wishlist wishlist = findWishlistByUserIdWithThrow(userId);
 
-    WishlistBrand wishlistBrand = findWishlistBrandByWishlistIdWithThrow(
-        wishlist.getId());
-
     // 사용자가 요청한(좋아요) 브랜드 인스턴스
     Brand brand = findBrandByIdWithThrow(brandId);
 
-    wishlistBrand.addBrand(brand, wishlist);
+    List<WishlistBrand> wishlistBrands = wishlist.getWishlistBrands();
+
+    wishlistBrands.add(WishlistBrand.of(brand, wishlist));
+
     wishlistRepository.save(wishlist);
   }
 
   @Override
   public void remove(Long userId, Long brandId) {
-    Wishlist wishlist = findWishlistByUserIdWithThrow(userId);
+    WishlistBrand wishlistBrand = findWishlistBrandByBrandIdWithThrow(brandId);
 
-    WishlistBrand wishlistBrand = findWishlistBrandByWishlistIdWithThrow(
-        wishlist.getId());
-
-    // 사용자가 요청한(좋아요) 브랜드 인스턴스
-    Brand brand = findBrandByIdWithThrow(brandId);
-
-    wishlistBrand.removeBrand(brand);
     wishlistBrandRepository.delete(wishlistBrand);
   }
 
@@ -52,26 +47,35 @@ public class WishlistBrandService implements WishlistService {
     // 로그인한 자신의 id로 자신의 위시리스트(좋아요 목록)을 조회
     Wishlist wishlist = findWishlistByUserIdWithThrow(userId);
 
-    // 위시리스트 id로 위시리스트에 "담겨 있는" 상품 목록 조회
-    WishlistBrand wishlistBrand = findWishlistBrandByWishlistIdWithThrow(wishlist.getId());
+    // 위시리스트에 "담겨 있는" 브랜드 목록 조회
+    List<WishlistBrand> wishlistBrands = wishlist.getWishlistBrands();
 
-    List<Brand> brands = wishlistBrand.getBrands();
+    // 브랜드 위시리스트에 담겨 있는 브랜드 객체 추출
+    List<Brand> brands = wishlistBrands.stream()
+        .map(WishlistBrand::getBrand)
+        .toList();
 
     return FindWishlistBrandsResponse.from(brands);
   }
 
   private Wishlist findWishlistByUserIdWithThrow(Long userId) {
     return wishlistRepository.findByUserId(userId)
-        .orElseThrow(() -> new IllegalArgumentException());
+        .orElseThrow(() -> new BusinessException(ErrorCode.WISHLIST_NOT_FOUND));
   }
 
   private WishlistBrand findWishlistBrandByWishlistIdWithThrow(Long wishlistId) {
     return wishlistBrandRepository.findById(wishlistId)
-        .orElseThrow(() -> new IllegalArgumentException());
+        .orElseThrow(() -> new BusinessException(ErrorCode.WISHLIST_BRAND_NOT_FOUND));
   }
 
   private Brand findBrandByIdWithThrow(Long brandId) {
     return brandRepository.findById(brandId)
-        .orElseThrow(() -> new IllegalArgumentException());
+        .orElseThrow(() -> new BusinessException(ErrorCode.BRAND_NOT_FOUND));
+  }
+
+  private WishlistBrand findWishlistBrandByBrandIdWithThrow(Long brandId) {
+    WishlistBrand wishlistBrand = wishlistBrandRepository.findByBrandId(brandId)
+        .orElseThrow(() -> new BusinessException(ErrorCode.WISHLIST_BRAND_NOT_FOUND));
+    return wishlistBrand;
   }
 }
--- src/main/java/com/devcrew/moodcode/domain/wishlist/service/WishlistProductService.java
+++ src/main/java/com/devcrew/moodcode/domain/wishlist/service/WishlistProductService.java
@@ -7,9 +7,12 @@ import com.devcrew.moodcode.domain.wishlist.repository.ProductRepository;
 import com.devcrew.moodcode.domain.wishlist.repository.WishlistProductRepository;
 import com.devcrew.moodcode.domain.wishlist.repository.WishlistRepository;
 import com.devcrew.moodcode.domain.wishlist.service.response.FindWishlistProductsResponse;
+import com.devcrew.moodcode.global.error.ErrorCode;
+import com.devcrew.moodcode.global.error.exception.BusinessException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,59 +22,68 @@ public class WishlistProductService implements WishlistService{
   private final WishlistRepository wishlistRepository;
   private final WishlistProductRepository wishlistProductRepository;
 
+  @Transactional
   @Override
   public void like(Long userId, Long productId) {
     Wishlist wishlist = findWishlistByUserIdWithThrow(userId);
+    List<WishlistProduct> wishlistProducts = wishlist.getWishlistProducts();
 
-    WishlistProduct wishlistProduct = findWishlistProductByWishlistIdWithThrow(wishlist.getId());
-
-    // 사용자가 요청한(좋아요) 상품 인스턴스
+    // 사용자가 "좋아요" 요청한 상품 인스턴스
     Product product = findProductByIdWithThrow(productId);
 
-    wishlistProduct.addProduct(product, wishlist);
+    wishlistProducts.add(WishlistProduct.of(product, wishlist));
+
     wishlistRepository.save(wishlist);
   }
 
+
+  @Transactional
   @Override
   public void remove(Long userId, Long productId) {
-    Wishlist wishlist = findWishlistByUserIdWithThrow(userId);
-
-    WishlistProduct wishlistProduct = findWishlistProductByWishlistIdWithThrow(wishlist.getId());
-
-    // 사용자가 요청한(좋아요) 상품 인스턴스
+    // 사용자가 "삭제" 요청한 상품 인스턴스
     Product product = findProductByIdWithThrow(productId);
 
-    wishlistProduct.removeProduct(product);
-    wishlistProductRepository.delete(wishlistProduct); // 연관된 레코드 싹다 삭제됨. cascade
+    WishlistProduct wishlistProduct = findWishlistProductByProductIdWithThrow(product);
 
+    wishlistProductRepository.delete(wishlistProduct); // 연관된 레코드 싹다 삭제됨. cascade
   }
 
+  @Transactional(readOnly = true)
   @Override
   public FindWishlistProductsResponse getWishlist(Long userId) {
     // 로그인한 자신의 id로 자신의 위시리스트(좋아요 목록)을 조회
     Wishlist wishlist = findWishlistByUserIdWithThrow(userId);
 
-    // 위시리스트 id로 위시리스트에 "담겨 있는" 상품 목록 조회
-    WishlistProduct wishlistProduct = findWishlistProductByWishlistIdWithThrow(wishlist.getId());
+    // 위시리스트에 "담겨 있는" 상품 목록 조회
+    List<WishlistProduct> wishlistProducts = wishlist.getWishlistProducts();
 
-    List<Product> products = wishlistProduct.getProducts();
+    // wishlistProducts에 "담겨 있는" 상품의 객체만 추출
+    List<Product> products = wishlistProducts.stream()
+        .map(WishlistProduct::getProduct) // wishlistProduct.getProduct() 임.
+        .toList();
 
     return FindWishlistProductsResponse.from(products);
   }
 
-  private WishlistProduct findWishlistProductByWishlistIdWithThrow(Long wishlistId) {
-    return wishlistProductRepository.findById(wishlistId)
-        .orElseThrow(() -> new IllegalArgumentException());
+  private List<WishlistProduct> findAllByWishlistId(Long wishlistId) {
+    return wishlistProductRepository.findAllByWishlistId(
+        wishlistId);
   }
 
   private Wishlist findWishlistByUserIdWithThrow(Long userId) {
     return wishlistRepository.findByUserId(userId)
-        .orElseThrow(() -> new IllegalArgumentException());
+        .orElseThrow(() -> new BusinessException(ErrorCode.WISHLIST_NOT_FOUND));
   }
 
   private Product findProductByIdWithThrow(Long productId) {
     return productRepository.findById(productId)
-        .orElseThrow(() -> new IllegalArgumentException());
+        .orElseThrow(() -> new BusinessException(ErrorCode.PRODUCT_NOT_FOUND));
+  }
+
+  private WishlistProduct findWishlistProductByProductIdWithThrow(Product product) {
+    WishlistProduct wishlistProduct = wishlistProductRepository.findByProductId(product.getId())
+        .orElseThrow(() -> new BusinessException(ErrorCode.WISHLIST_PRODUCT_NOT_FOUND));
+    return wishlistProduct;
   }
 }
 
