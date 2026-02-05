6554637f04a6ae5f49d4c5a218e0beb2c8dd9b04
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-02-05 16:36:54 +0900

    [MOODCODE] feat: 조회 기능 추가
    - wishlistId로 해당하는 레코드의 WishlistProduct 객체 리스트 조회
    - productId로 해당하는 레코드의 WishlistProduct 객체 조회

--- src/main/java/com/devcrew/moodcode/domain/wishlist/repository/WishlistProductRepository.java
+++ src/main/java/com/devcrew/moodcode/domain/wishlist/repository/WishlistProductRepository.java
@@ -1,11 +1,14 @@
 package com.devcrew.moodcode.domain.wishlist.repository;
 
 import com.devcrew.moodcode.domain.wishlist.WishlistProduct;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WishlistProductRepository extends JpaRepository<WishlistProduct, Long> {
 
-
+  List<WishlistProduct> findAllByWishlistId(Long wishlistId);
+  Optional<WishlistProduct> findByProductId(Long productId);
 }
