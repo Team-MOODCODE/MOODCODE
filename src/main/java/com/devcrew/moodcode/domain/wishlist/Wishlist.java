1bdb6735d4c3d8f80b2fa6b520ae4c5fe6daab98
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-02-05 16:33:55 +0900

    [MOODCODE] refactor: wishlistProducts, wishlistBrands 필드 수정

--- src/main/java/com/devcrew/moodcode/domain/wishlist/Wishlist.java
+++ src/main/java/com/devcrew/moodcode/domain/wishlist/Wishlist.java
@@ -1,7 +1,7 @@
 package com.devcrew.moodcode.domain.wishlist;
 
 import com.devcrew.moodcode.domain.cart.User;
-import com.devcrew.moodcode.global.common.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,23 +9,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 
 @Entity
 @Getter
-public class Wishlist extends BaseTimeEntity {
+public class Wishlist {
   @Id @Column(name = "wishlist_id")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "wishlist_product_id", nullable = true)
-  private WishlistProduct wishlistProduct;
+  @OneToMany(mappedBy = "wishlist", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<WishlistProduct> wishlistProducts = new ArrayList<>();
 
-  @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "brand_id", nullable = true)
-  private Brand brand;
+  @OneToMany(mappedBy = "wishlist", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<WishlistBrand> wishlistBrands = new ArrayList<>();
 
   @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id")
