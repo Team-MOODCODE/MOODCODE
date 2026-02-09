5eb7e47407b740859c5d7a954c63abf9da510895
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-02-09 18:25:31 +0900

    [MOODCODE] feat: ResponseEntity 적용 및 DTO 유효성 검증 추가(@Valid)

--- src/main/java/com/devcrew/moodcode/domain/cart/controller/CartController.java
+++ src/main/java/com/devcrew/moodcode/domain/cart/controller/CartController.java
@@ -1,16 +1,13 @@
 package com.devcrew.moodcode.domain.cart.controller;
 
-import com.devcrew.moodcode.domain.cart.controller.command.AddCartItemCommand;
-import com.devcrew.moodcode.domain.cart.controller.command.FindCartItemCommand;
-import com.devcrew.moodcode.domain.cart.controller.command.RemoveCartItemCommand;
-import com.devcrew.moodcode.domain.cart.controller.command.UpdateItemCommand;
-import com.devcrew.moodcode.domain.cart.controller.request.AddItemRequest;
-import com.devcrew.moodcode.domain.cart.controller.request.UpdateItemRequest;
+import com.devcrew.moodcode.domain.cart.dto.AddCartItemRequest;
+import com.devcrew.moodcode.domain.cart.dto.UpdateCartItemRequest;
 import com.devcrew.moodcode.domain.cart.service.CartService;
-import com.devcrew.moodcode.domain.cart.service.response.FindCartItemsResponse;
-import com.devcrew.moodcode.global.api.Api;
+import com.devcrew.moodcode.domain.cart.dto.FindCartItemsResponse;
+import com.devcrew.moodcode.global.auth.LoginUser;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,66 +23,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartController {
   private final CartService cartService;
 
-  /*
-  UserDTO는 사용자가 로그인한 계정의 정보를 가지고 있는 DTO입니다. 임시로 작성한 것.
-  User쪽 구현이 완료되면 수정할 예정입니다.
-  클래스를 따로 만들지 않았습니다.
-   */
-
   @PostMapping("/items")
-  public void addCartItem(
-//      @AuthenticationPrincipal UserDTO userDTO,
-      @RequestBody @Validated AddItemRequest request) {
-    Long userId = 1L; // 임시
+  public ResponseEntity<Void> addCartItem(
+      @LoginUser Long userId,
+      @RequestBody @Valid AddCartItemRequest request) {
 
-    AddCartItemCommand command = AddCartItemCommand.of(
-//        userDTO.getId(),
-        userId,
-        request.productOptionId());
-    cartService.addCartItem(command);
+    cartService.addCartItem(userId, request.toCommand());
+    return ResponseEntity.noContent().build();
   }
 
   @GetMapping("/items")
-  public Api<FindCartItemsResponse> getCartItems(
-//      @AuthenticationPrincipal UserDTO userDTO
+  public ResponseEntity<FindCartItemsResponse> getCartItems(
+      @LoginUser Long userId
   ) {
 
-    Long userId = 1L; // 임시
-    FindCartItemCommand command = FindCartItemCommand.from(
-//        userDTO.getId(),
-        userId);
-    return Api.ok(cartService.getCartItems(command));
+    return ResponseEntity.ok(cartService.getCartItems(userId));
   }
 
   @PatchMapping("/items/{cartItemId}")
-  public void updateCartItem(
-//      @AuthenticationPrincipal UserDTO userDTO,
+  public ResponseEntity<Void> updateCartItem(
+      @LoginUser Long userId,
       @PathVariable Long cartItemId,
-      @RequestBody UpdateItemRequest request) {
+      @RequestBody @Valid UpdateCartItemRequest request) {
 
-    Long userId = 1L; // 임시
-
-    UpdateItemCommand command = UpdateItemCommand.of(
-//        userDTO.getId(),
-        userId,
-        cartItemId,
-        request.optionName(),
-        request.count());
-    cartService.updateCartItem(command);
+    cartService.updateCartItem(userId, cartItemId, request.toCommand());
+    return ResponseEntity.noContent().build();
   }
 
   @DeleteMapping("/items/{cartItemId}")
-  public void removeCartItem(
-//      @AuthenticationPrincipal UserDTO userDTO,
+  public ResponseEntity<Void> removeCartItem(
+      @LoginUser Long userId,
       @PathVariable Long cartItemId) {
 
-    Long userId = 1L; // 임시
-
-    RemoveCartItemCommand command = RemoveCartItemCommand.of(
-//        userDTO.getId(),
-        userId,
-        cartItemId);
-    cartService.removeCartItem(command);
+    cartService.removeCartItem(userId, cartItemId);
+    return ResponseEntity.noContent().build();
   }
 
 }
