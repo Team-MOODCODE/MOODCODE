1525b40843692557853584c1505e6c124c62467d
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-02-09 18:31:05 +0900

    [MOODCODE] refactor: 중첩 record 가독성 향상 및 createdAt 필드 추가 구현

--- src/main/java/com/devcrew/moodcode/domain/cart/dto/FindCartItemsResponse.java
+++ src/main/java/com/devcrew/moodcode/domain/cart/dto/FindCartItemsResponse.java
@@ -1,11 +1,14 @@
-package com.devcrew.moodcode.domain.cart.service.response;
+package com.devcrew.moodcode.domain.cart.dto;
 
 import com.devcrew.moodcode.domain.cart.CartItem;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
 
-public record FindCartItemsResponse(List<FindCartItemResponse> findCartItemResponse, Integer totalPrice) {
+public record FindCartItemsResponse(
+    List<FindCartItemResponse> findCartItemResponse,
+    Integer totalPrice
+) {
 
   public static FindCartItemsResponse from(
       List<CartItem> cartItems) {
@@ -16,6 +19,7 @@ public record FindCartItemsResponse(List<FindCartItemResponse> findCartItemRespo
               .optionName(cartItem.getProductOption().getOptionName())
               .count(cartItem.getCount())
               .updatedAt(cartItem.getUpdatedAt())
+              .createdAt(cartItem.getCreatedAt())
               .productOptionId(cartItem.getProductOption().getId())
               .cartId(cartItem.getCart().getId()).build();
         }).toList(),
@@ -23,7 +27,15 @@ public record FindCartItemsResponse(List<FindCartItemResponse> findCartItemRespo
     );
   }
   @Builder
-  public record FindCartItemResponse(Long cartItemId, String optionName, int count, LocalDateTime updatedAt, Long productOptionId, Long cartId) {
+  public record FindCartItemResponse(
+      Long cartItemId,
+      String optionName,
+      int count,
+      LocalDateTime updatedAt,
+      LocalDateTime createdAt,
+      Long productOptionId,
+      Long cartId
+  ) {
 
   }
 }
